f673a8ccce3e8ee81cabb9845f1286f133b4366b
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2024-07-16 15:36:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Count of smaller elements/count-of-smaller-elements.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countOfElements(int x, vector<int> &arr) {
+
+        // Code Here
+        int c=0;
+        for (int i=0;i<arr.size();i++){
+            if(arr[i]<=x){
+                c+=1;
+            }
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        scanf("%d ", &x);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.countOfElements(x, arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
