d52f41a02ce8556c472d145a0decfbc5b9756bc4
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2024-07-17 19:52:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Display longest name/display-longest-name.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cin >> A[i];
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i]<< " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string longest(int n, vector<string> &names) {
+        // code here
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(names[i].length()>names[index].length()){
+                index=i;
+            }
+        }
+        return names[index];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<string> names(n);
+        Array::input(names,n);
+        
+        Solution obj;
+        string res = obj.longest(n, names);
+        
+        cout<<res<<"\n";
+        
+    }
+}
+
+// } Driver Code Ends
