d30a92114ce51cb4f33788d4564391e6228c8bb4
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2024-07-19 14:51:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Delete alternate characters/delete-alternate-characters.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string S) {
+        // code here
+        string res;
+        res+=S[0];
+        for(int i=1;i<S.size();i++){
+            if(i%2==0){
+                res+=S[i];
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
