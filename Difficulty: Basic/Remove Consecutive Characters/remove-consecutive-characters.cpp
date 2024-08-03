4e14be95f5e5f02616b11c1e7d995fdb8a5aeb57
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2024-08-03 11:59:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Consecutive Characters/remove-consecutive-characters.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+        // code here.
+        string res=S;
+        S.clear();
+        for(int i=0;i<res.size();i++){
+            if(res[i]==res[i-1]) continue;
+            else S+=res[i];
+        }
+        return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
