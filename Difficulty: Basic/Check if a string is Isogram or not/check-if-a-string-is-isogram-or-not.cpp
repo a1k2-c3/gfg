bbafecf5022ab044d382307757259fc647705acb
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2024-08-08 11:56:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check if a string is Isogram or not/check-if-a-string-is-isogram-or-not.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if a string is Isogram or not.
+    bool isIsogram(string s)
+    {
+        //Your code here
+        unordered_map<char,int>mp;
+        for(auto i:s)
+        {
+            mp[i]+=1;
+        }
+        for(auto i:mp)
+        {
+            // cout<<i.first<<" "<<i.second<<endl;
+            if(i.second>1) return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.isIsogram(s)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
