79eedcd679633575085dbfc1ae80f79ca5c78d57
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2024-08-19 11:46:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fighting the darkness/fighting-the-darkness.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int maxDays(vector<int> &arr) {
+        // code here
+        int res=*max_element(arr.begin(),arr.end());
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxDays(arr) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
