1d7600feb419ab43afd0c064a39e85b155be3ebf
Akhilesh Varanasi <22a91a0459@aec.edu.in>
2025-01-22 17:09:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Linked List/print-linked-list.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+/*
+    Print elements of a linked list on console
+    Head pointer input could be NULL as well for empty list
+*/
+
+class Solution {
+  public:
+    // Function to display the elements of a linked list in same line
+    void printList(Node *head) {
+        // your code goes here
+        Node*temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        Node *head = nullptr, *tail = nullptr;
+        int x;
+
+        // Building the linked list from input
+        while (ss >> x) {
+            if (head == nullptr) {
+                head = new Node(x);
+                tail = head;
+            } else {
+                tail->next = new Node(x);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        ob.printList(head);
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
